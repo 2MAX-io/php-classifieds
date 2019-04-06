f4d75590f94fb54242bcccaa5f3d2aee0a711ad3
LPodolski <m@lpodolski.com>
2019-04-06 10:36:51 +0200

    add index

---
+++ symfony/src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}

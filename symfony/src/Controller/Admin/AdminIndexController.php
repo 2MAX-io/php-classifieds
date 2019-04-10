1518b4956ff6fb60089603ce75bec384de4457df
LPodolski <m@lpodolski.com>
2019-04-10 09:13:38 +0200

    admin basic security setup

---
+++ symfony/src/Controller/Admin/AdminIndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminIndexController extends AbstractController
+{
+    /**
+     * @Route("/admin/red5/index", name="app_admin_index")
+     * @Route("/admin/red5/")
+     */
+    public function validityExtend(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+        ]);
+    }
+}

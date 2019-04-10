fddcafee0aea4a45e55d39560efbbcd8fa6387d2
LPodolski <m@lpodolski.com>
2019-04-10 15:38:39 +0200

    better file upload look, listing add translations

--- symfony/src/Form/ListingType.php
+++ symfony/src/Form/ListingType.php
@@ -4,11 +4,15 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Listing;
+use App\Form\Type\FileSimpleType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
@@ -18,37 +22,60 @@ class ListingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description', TextareaType::class)
+            ->add('title', TextType::class, [
+                'label' => 'trans.Title'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'trans.Description'
+            ])
             ->add(
                 'category',
                 EntityType::class,
                 [
                     'choice_label' => 'name', 'class' => Category::class,
                     'placeholder' => 'trans.Select category',
+                    'label' => 'trans.Category'
                 ]
             )
-            ->add('price')
-            ->add('phone')
-            ->add('email')
+            ->add('price', IntegerType::class, [
+                'label' => 'trans.Price'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'trans.Phone'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'trans.Email'
+            ])
             ->add('validityTimeDays', ChoiceType::class, [
                 'mapped' => false,
-                'placeholder' => 'trans.Chose validityTime',
                 'choices' => $this->getValidityTimeDaysChoices(),
                 'constraints' => [
                     new Constraints\Choice([
                         'choices' => $this->getValidityTimeDaysChoices()
                     ]),
                 ],
+                'label' => 'trans.Validity time',
+                'data' => 9,
             ])
-            ->add('city')
-            ->add('file', FileType::class, [
+            ->add('city', TextType::class, [
+                'label' => 'trans.City'
+            ])
+            ->add('customFields', HiddenType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'formCustomFieldsHidden'
+                ]
+            ])
+            ->add('file', FileSimpleType::class, [
                 'mapped' => false,
                 'required' => false,
                 'multiple' => true,
                 'constraints' => [
 //                    new Assert\File(['mimeTypes']) // todo: mime validation
-                ]
+                ],
+                'attr' => ['hidden' => 'hidden'],
+                'label' => 'trans.Pictures',
+                'block_name' => 'simple',
             ])
         ;
     }

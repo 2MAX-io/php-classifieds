5ac4493e1365f3da2ad75a3365e66ed2a5832115
LPodolski <m@lpodolski.com>
2019-04-15 22:13:32 +0200

    email better messages

--- symfony/src/Service/Email/EmailService.php
+++ symfony/src/Service/Email/EmailService.php
@@ -109,6 +109,7 @@ class EmailService
                     [
                         'user' => $user,
                         'newEmail' => $newEmail,
+                        'oldEmail' => $user->getEmail(),
                     ]
                 ),
                 'text/html'

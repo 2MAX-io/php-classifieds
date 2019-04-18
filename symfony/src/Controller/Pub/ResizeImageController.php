2009721fdb964e0275c659b6bc3a893618504be2
LPodolski <m@lpodolski.com>
2019-04-18 17:53:09 +0200

    basic on demand resize

--- symfony/src/Controller/Pub/ResizeImageController.php
+++ symfony/src/Controller/Pub/ResizeImageController.php
@@ -6,10 +6,12 @@ namespace App\Controller\Pub;
 
 use App\Helper\FilePath;
 use App\System\Glide\AppServerFactory;
+use Intervention\Image\Exception\NotFoundException;
 use League\Glide\Responses\SymfonyResponseFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Webmozart\PathUtil\Path;
 
 class ResizeImageController
 {
@@ -20,13 +22,28 @@ class ResizeImageController
     {
 //        return new Response('image resize '. $path . '/' . $file);
 
+        if (!in_array(Path::getExtension($file, true), ['jpg', 'png', 'gif', 'jpeg'])) {
+            throw new NotFoundException();
+        }
+
         $server = AppServerFactory::create([
             'source' => FilePath::getStaticPath(),
-            'cache' => FilePath::getStaticCachePath(),
+            'cache' => FilePath::getStaticPath(),
+            'cache_path_prefix' => 'cache',
             'response' => new SymfonyResponseFactory($request)
         ]);
 
-        return $server->getImageResponse($path . '/' . $file, $this->getParams($type));
+        $targetPath = Path::canonicalize(FilePath::getProjectDir() . $request->getRequestUri());
+
+        if (!in_array(Path::getExtension($targetPath, true), ['jpg', 'png', 'gif', 'jpeg'])) {
+            throw new NotFoundException();
+        }
+
+        $cachedPath = $server->makeImage($path . '/' . $file, $this->getParams($type));
+
+        rename(Path::canonicalize(FilePath::getStaticPath() . '/' . $cachedPath), $targetPath);
+
+        return $server->getResponseFactory()->create($server->getCache(), Path::makeRelative($targetPath, FilePath::getStaticPath()));
     }
 
     private function getParams(string $type): array

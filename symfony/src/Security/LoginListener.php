26fcc69487e33f40a796d8d2b70f779c5c74d6d5
LPodolski <m@lpodolski.com>
2019-04-23 15:35:11 +0200

    migrate old password to new

--- symfony/src/Security/LoginListener.php
+++ symfony/src/Security/LoginListener.php
@@ -5,16 +5,24 @@ declare(strict_types=1);
 namespace App\Security;
 
 use App\Entity\User;
+use App\Helper\Random;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener
 {
     private $em;
 
-    public function __construct(EntityManagerInterface $em)
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $userPasswordEncoder)
     {
         $this->em = $em;
+        $this->userPasswordEncoder = $userPasswordEncoder;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
@@ -23,6 +31,9 @@ class LoginListener
 
         if ($userInterface instanceof User) {
             $this->user($userInterface);
+            if ($userInterface->getEncoderName() !== null && $event->getRequest()->request->get('password', false)) {
+                $this->migrateLegacyPassword($userInterface, $event->getRequest()->request->get('password'));
+            }
         }
 
         $this->em->flush();
@@ -31,6 +42,18 @@ class LoginListener
     private function user(User $user)
     {
         $user->setLastLogin(new \DateTime());
+        $this->em->persist($user);
+    }
+
+    private function migrateLegacyPassword(User $user, string $plainPassword)
+    {
+        if ($user->getEncoderName() === null) {
+            return;
+        }
+
+        $user->setPassword('removed ' . Random::string(20));
+        $user->setPassword($this->userPasswordEncoder->encodePassword($user, $plainPassword));
+
         $this->em->persist($user);
     }
 }

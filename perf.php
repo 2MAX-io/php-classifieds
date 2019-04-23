a7d74a6a284c802581d3e6c2ef5d782962b512ac
LPodolski <m@lpodolski.com>
2019-04-23 17:16:40 +0200

    basic perf handling

---
+++ perf.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+require __DIR__ . '/symfony/config/bootstrap.php';
+
+define('PUBLIC_DIR_LOCATION', __DIR__);
+define('SYMFONY_LOCATION', __DIR__ . '/symfony');
+
+$loader = new \Twig\Loader\FilesystemLoader(SYMFONY_LOCATION . '/templates');
+$twig = new \Twig\Environment($loader, [
+//    'cache' => SYMFONY_LOCATION . '/var/cache/perf_app/twig',
+]);
+
+$twig->addFilter(new TwigFilter('trans', 'fooReturnSame'));
+$twig->addFunction(new TwigFunction('path', 'fooReturnSame'));
+$twig->addFunction(new TwigFunction('asset', 'fooReturnSame'));
+$twig->addFunction(new TwigFunction('lowSecurityCheckIsAdminInPublic', function() {
+    return false;
+}));
+
+echo $twig->render('index.html.twig', ['name' => 'zz']);
+
+
+function fooReturnSame($val) {
+    return $val;
+}

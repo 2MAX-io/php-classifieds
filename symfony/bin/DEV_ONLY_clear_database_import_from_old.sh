1df4559bfda9e487ef0cef5d6526bffd6ec3df59
LPodolski <m@lpodolski.com>
2019-04-24 08:44:32 +0200

    migrate from old, improved script

--- symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
+++ symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-php symfony/bin/importer_export_from_old.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv
-php symfony/bin/importer_import_to_new.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv /var/www/html/symfony/docker/mysql/sql/importer_import_to_new.sql
+docker exec classifieds_mysql bash -c "mysqldump classifieds > /sql/ogl_$(date +%Y-%m-%d_%H%M%S)_old.sql"
+docker exec classifieds_mysql mysql -e "DROP DATABASE admin_ogloszenia"
+docker exec classifieds_mysql mysql -e "CREATE DATABASE admin_ogloszenia"
+docker exec classifieds_mysql bash -c "mysql admin_ogloszenia < /sql/admin_ogloszenia.sql"
 
-mysql < symfony/dev/sql_snippets/truncate_listings.sql
+docker exec classifieds_php bash -c "php symfony/bin/importer_export_from_old.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv"
+docker exec classifieds_php bash -c "php symfony/bin/importer_import_to_new.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv /var/www/html/symfony/docker/mysql/sql/importer_import_to_new.sql"
 
-time mysql classifieds < /sql/importer_import_to_new.sql
+docker exec classifieds_mysql bash -c "mysql classifieds < /sql/dev/truncate_listings.sql"
+time docker exec classifieds_mysql bash -c "mysql classifieds < /sql/importer_import_to_new.sql"
+
+docker exec classifieds_mysql bash -c "mysqldump classifieds > /sql/ogl_$(date +%Y-%m-%d_%H%M%S)_new.sql"
+

75434cdf0c82cf29195731480ad5598a6faa3c33
LPodolski <m@lpodolski.com>
2019-05-01 08:36:14 +0200

    better dev, stage deploy scripts

--- symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
+++ symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
@@ -8,7 +8,37 @@ docker exec classifieds_mysql bash -c "mysql admin_ogloszenia < /sql/admin_oglos
 docker exec classifieds_php bash -c "php symfony/bin/importer_export_from_old.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv"
 docker exec classifieds_php bash -c "php symfony/bin/importer_import_to_new.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv /var/www/html/symfony/docker/mysql/sql/importer_import_to_new.sql"
 
-docker exec classifieds_mysql bash -c "mysql classifieds < /sql/dev/truncate_listings.sql"
+
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_file"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_view"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE user_balance_change"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_listing_police_log"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_custom_field_value"
+
+
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE admin"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE category"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field_category"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field_option"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE featured_package"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE featured_package_for_category"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_custom_field_value"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE page"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE setting"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE user"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_listing_police_log"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token"
+#docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token_field"
+
+docker exec classifieds_php php symfony/bin/console doctrine:schema:update --force
+
 time docker exec classifieds_mysql bash -c "mysql classifieds < /sql/importer_import_to_new.sql"
 
+docker exec classifieds_php php symfony/bin/console app:cron:main
+
 docker exec classifieds_mysql bash -c "mysqldump classifieds > /sql/ogl_$(date +%Y-%m-%d_%H%M%S)_new.sql"
+
+bash DEV_pull_images.sh
---
+++ symfony/bin/STAGING_deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+cd /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/
+
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE admin"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE category"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field_category"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE custom_field_option"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE featured_package"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE featured_package_for_category"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_custom_field_value"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_file"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_view"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE page"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE setting"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE user"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE user_balance_change"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_listing_police_log"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token"
+mysql lpodnogl_ogl -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token_field"
+
+mysql lpodnogl_ogl < /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony/var/ogl.sql
+
+
+
+git pull
+/usr/local/php73/bin/php /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/composer.phar install -d /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony
+rm -r /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony/var/cache/prod/
+/usr/local/php73/bin/php symfony/bin/console d:schema:update --force
+/usr/local/php73/bin/php /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/composer.phar dump-env prod -d /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony
+/usr/local/php73/bin/php /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/composer.phar dump-autoload --optimize -d /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony
+/usr/local/php73/bin/php /home/lpodnogl/domains/oglnew-hjk1.lpodolski.com/public_html/symfony/bin/console app:cron:main

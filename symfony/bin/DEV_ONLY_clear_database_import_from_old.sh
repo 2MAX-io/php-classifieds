7d310cac484f7d7cb58cc589ab3375b970237238
LPodolski <m@lpodolski.com>
2019-05-02 21:53:53 +0200

    fix import old command, cf disable for translations

--- symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
+++ symfony/bin/DEV_ONLY_clear_database_import_from_old.sh
@@ -8,14 +8,17 @@ docker exec classifieds_mysql bash -c "mysql admin_ogloszenia < /sql/admin_oglos
 docker exec classifieds_php bash -c "php symfony/bin/importer_export_from_old.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv"
 docker exec classifieds_php bash -c "php symfony/bin/importer_import_to_new.php /var/www/html/symfony/docker/mysql/sql/importer_export_from_old.csv /var/www/html/symfony/docker/mysql/sql/importer_import_to_new.sql"
 
-
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_file"
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_view"
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing"
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE user_balance_change"
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_listing_police_log"
 docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE listing_custom_field_value"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE payment_featured_package"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE payment"
+docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE payment_for_balance_top_up"
 
+docker exec classifieds_php php symfony/bin/console doctrine:schema:update --force
 
 #docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE admin"
 #docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE category"
@@ -33,12 +36,10 @@ docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; 
 #docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token"
 #docker exec classifieds_mysql mysql classifieds -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE zzzz_token_field"
 
-docker exec classifieds_php php symfony/bin/console doctrine:schema:update --force
-
 time docker exec classifieds_mysql bash -c "mysql classifieds < /sql/importer_import_to_new.sql"
 
 docker exec classifieds_php php symfony/bin/console app:cron:main
 
 docker exec classifieds_mysql bash -c "mysqldump classifieds > /sql/ogl_$(date +%Y-%m-%d_%H%M%S)_new.sql"
 
-bash DEV_pull_images.sh
+bash /home/u/PhpstormProjects/classifieds/symfony/bin/DEV_pull_images.sh

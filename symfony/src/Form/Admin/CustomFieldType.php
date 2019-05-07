0d2aecbdc11acf6b8fad70cb5102c48ad6538f33
LPodolski <m@lpodolski.com>
2019-05-07 14:53:12 +0200

    admin custom fields - improved crud with translations

--- symfony/src/Form/Admin/CustomFieldType.php
+++ symfony/src/Form/Admin/CustomFieldType.php
@@ -6,6 +6,9 @@ namespace App\Form\Admin;
 
 use App\Entity\CustomField;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,24 @@ class CustomFieldType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
-        $builder->add('type');
-        $builder->add('required');
-        $builder->add('searchable');
-        $builder->add('unit');
+        $builder->add('name', TextType::class, [
+            'label' => 'trans.Name',
+        ]);
+        $builder->add('type', ChoiceType::class, [
+            'label' => 'trans.Type',
+            'choices' => [
+
+            ],
+        ]);
+        $builder->add('required', CheckboxType::class, [
+            'label' => 'trans.Required?',
+        ]);
+        $builder->add('searchable', CheckboxType::class, [
+            'label' => 'trans.Searchable?',
+        ]);
+        $builder->add('unit', TextType::class, [
+            'label' => 'trans.Unit',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

059339be7a86dad89963c463d3f12a6aa14e8568
LPodolski <m@lpodolski.com>
2019-05-15 21:57:18 +0200

    mass action - basic custom field - working but hardcoded

--- symfony/src/Service/Admin/Listing/ExecuteActionOnFiltered/ExecuteActionOnFilteredService.php
+++ symfony/src/Service/Admin/Listing/ExecuteActionOnFiltered/ExecuteActionOnFilteredService.php
@@ -29,7 +29,22 @@ class ExecuteActionOnFilteredService
     public function addCustomField(): void
     {
         $qb = $this->adminListingSearchService->getQuery();
-        $qb->select("listing.id, 1, 2, 'fiat'");
+        $qb->select("listingCustomFieldValue.id");
+        $qb->addSelect("listing.id");
+        $qb->leftJoin('listing.listingCustomFieldValues', 'listingCustomFieldValue');
+
+        $qb->andWhere($qb->expr()->orX(
+            $qb->expr()->andX(
+                $qb->expr()->eq('listingCustomFieldValue.customField', ':customField'),
+                $qb->expr()->neq('listingCustomFieldValue.customFieldOption', ':customFieldOption'),
+                $qb->expr()->neq('listingCustomFieldValue.value', ':value')
+            ),
+            $qb->expr()->isNull('listingCustomFieldValue.id')
+        ));
+        $qb->setParameter('customField', 1);
+        $qb->setParameter('customFieldOption', 2);
+        $qb->setParameter('value', 'fiat');
+
         $qb->andWhere($qb->expr()->eq('listing.id', 412490));
         $qb->setMaxResults(1);
         $qb->resetDQLPart('orderBy');
@@ -39,16 +54,19 @@ class ExecuteActionOnFilteredService
         $qb->getParameters()->toArray();
         time();
 
-        $params = [];
+        $params = [1, 2, 'fiat'];
         /** @var Parameter[] $doctrineQueryParamList */
         $doctrineQueryParamList = $qb->getParameters()->toArray();
         foreach ($doctrineQueryParamList as $key => $doctrineQueryParam) {
-            $params[$key] = $doctrineQueryParam->getValue();
+            $params[] = $doctrineQueryParam->getValue();
         }
+
+        $fields = ", ?, ?, ?";
+        $selectSql = \preg_replace('#SELECT(.+)FROM(.+)#', 'SELECT $1 '.$fields.' FROM $2', $selectSql);
 
         $pdo = $this->em->getConnection();
         $stmt = $pdo->prepare("
-INSERT INTO listing_custom_field_value (listing_id, custom_field_id, custom_field_option_id, value)
+INSERT INTO listing_custom_field_value (id, listing_id, custom_field_id, custom_field_option_id, value)
 $selectSql
 ");
         $stmt->execute($params);

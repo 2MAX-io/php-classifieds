52caca62b93a6c976f7d4fe88e9a0572ece58b63
LPodolski <m@lpodolski.com>
2019-05-15 12:17:25 +0200

    admin basic payment list - pagination

--- symfony/src/Controller/Admin/Payment/PaymentController.php
+++ symfony/src/Controller/Admin/Payment/PaymentController.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Admin\Payment;
 
 use App\Controller\Admin\Base\AbstractAdminController;
 use App\Entity\Payment;
-use App\Repository\PaymentRepository;
+use App\Service\Money\PaymentHistoryService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +16,15 @@ class PaymentController extends AbstractAdminController
     /**
      * @Route("/admin/red5/payment/", name="app_admin_payment_index", methods={"GET"})
      */
-    public function index(PaymentRepository $paymentRepository): Response
+    public function index(Request $request, PaymentHistoryService $paymentHistoryService): Response
     {
         $this->denyUnlessAdmin();
 
+        $paginationDto = $paymentHistoryService->getPaymentList((int) $request->get('page', 1));
+
         return $this->render('admin/payment/index.html.twig', [
-            'payments' => $paymentRepository->findAll(),
+            'payments' => $paginationDto->getResults(),
+            'pager' => $paginationDto->getPager(),
         ]);
     }
 
---
+++ symfony/src/Service/Money/PaymentHistoryService.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Money;
+
+use App\Entity\Payment;
+use App\Service\System\Pagination\PaginationDto;
+use App\Service\System\Pagination\PaginationService;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PaymentHistoryService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var PaginationService
+     */
+    private $paginationService;
+
+    public function __construct(EntityManagerInterface $em, PaginationService $paginationService)
+    {
+        $this->em = $em;
+        $this->paginationService = $paginationService;
+    }
+
+    public function getPaymentList(int $page): PaginationDto
+    {
+        $qb = $this->em->getRepository(Payment::class)->createQueryBuilder('payment');
+        $qb->orderBy('payment.id', 'DESC');
+
+        $pager = $this->paginationService->createPaginationForQb($qb);
+        $pager->setMaxPerPage($this->paginationService->getMaxPerPage());
+        $pager->setCurrentPage($page);
+
+        $paginationDto = new PaginationDto($pager->getCurrentPageResults(), $pager);
+
+        return $paginationDto;
+    }
+}

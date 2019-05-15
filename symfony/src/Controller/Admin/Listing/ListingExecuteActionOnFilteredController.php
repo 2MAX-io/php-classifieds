683d80fb1ec87b79842806dcdee609450a608bdc
LPodolski <m@lpodolski.com>
2019-05-15 19:13:16 +0200

    mass action - basic pages

---
+++ symfony/src/Controller/Admin/Listing/ListingExecuteActionOnFilteredController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin\Listing;
+
+use App\Controller\Admin\Base\AbstractAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListingExecuteActionOnFilteredController extends AbstractAdminController
+{
+    /**
+     * @Route("/admin/red5/listing/execute-action-on-filtered", name="app_admin_listing_execute_on_filtered")
+     */
+    public function executeActionOnFiltered(Request $request): Response
+    {
+        $this->denyUnlessAdmin();
+
+        return $this->render('admin/listing/execute_on_filtered/execute_on_filtered.html.twig', [
+
+        ]);
+    }
+}

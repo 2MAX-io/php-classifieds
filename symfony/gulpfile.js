d5e6ae7d3cb2778d9aa9d745f38f9cf1ffaaa94f
LPodolski <m@lpodolski.com>
2019-05-19 21:24:51 +0200

    gulp better build, execution order fixes

--- symfony/gulpfile.js
+++ symfony/gulpfile.js
@@ -3,27 +3,47 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const cleanCSS = require('gulp-clean-css');;
+const cleanCSS = require('gulp-clean-css');
+const rev = require('gulp-rev');
+const rename = require('gulp-rename');
 
 function css() {
   return src([
       'asset/bootstrap.css',
       'asset/style.css',
     ])
+    .pipe(concat('app.css'))
     .pipe(cleanCSS({level: {1: {specialComments: false}}}))
     .pipe(cssnano())
-    .pipe(concat('app.css'))
-    .pipe(dest('asset'))
+    // .pipe(rev())
+    // .pipe(dest('asset/build'))
+    // .pipe(rename({
+    //   dirname: "asset/build" // rename dir in manifest
+    // }))
+    // .pipe(rev.manifest('asset/build/rev-manifest.json', {
+    //   base: 'asset/build',
+    //   merge: true
+    // }))
+    .pipe(dest('asset/build'))
 }
 
 function cssBottom() {
   return src([
       'asset/lib/fancybox/jquery.fancybox.css',
     ])
-      .pipe(cleanCSS({level: {1: {specialComments: false}}}))
-      .pipe(cssnano())
     .pipe(concat('app.bottom.css'))
-    .pipe(dest('asset'))
+    .pipe(cleanCSS({level: {1: {specialComments: false}}}))
+    .pipe(cssnano())
+    // .pipe(rev())
+    // .pipe(dest('asset/build'))
+    // .pipe(rename({
+    //   dirname: "asset/build" // rename dir in manifest
+    // }))
+    // .pipe(rev.manifest('asset/build/rev-manifest.json', {
+    //   base: 'asset/build',
+    //   merge: true
+    // }))
+    .pipe(dest('asset/build'))
 }
 
 function js() {
@@ -37,10 +57,19 @@ function js() {
     'asset/main.js',
   ])
     .pipe(sourcemaps.init())
-    .pipe(uglify())
     .pipe(concat('app.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write('sourcemaps'))
-    .pipe(dest('asset'))
+    // .pipe(rev())
+    // .pipe(dest('asset/build'))
+    // .pipe(rename({
+    //   dirname: "asset/build" // rename dir in manifest
+    // }))
+    // .pipe(rev.manifest('asset/build/rev-manifest.json', {
+    //   base: 'asset/build',
+    //   merge: true
+    // }))
+    .pipe(dest('asset/build'))
 }
 
 exports.js = js;

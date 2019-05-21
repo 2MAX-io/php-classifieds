a8fa113721b0fa8d0d8fb0801563580012e1d902
LPodolski <m@lpodolski.com>
2019-05-21 10:29:23 +0200

    page fix order, better slug handling

--- symfony/src/Form/Admin/PageType.php
+++ symfony/src/Form/Admin/PageType.php
@@ -5,32 +5,44 @@ declare(strict_types=1);
 namespace App\Form\Admin;
 
 use App\Entity\Page;
+use App\Helper\Str;
 use App\Validator\Constraints\Slug;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PageType extends AbstractType
 {
+    const SLUG = 'slug';
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('slug', TextType::class, [
-            'label' => 'trans.Slug',
-            'constraints' => [
-                new NotBlank(),
-                new Slug(),
-            ],
-        ]);
         $builder->add('title', TextType::class, [
             'label' => 'trans.Title',
             'constraints' => [
                 new NotBlank(),
             ],
         ]);
+        $builder->add(
+            self::SLUG, TextType::class, [
+            'label' => 'trans.Slug',
+            'constraints' => [
+                new NotBlank(),
+                new Slug(),
+            ],
+        ])
+        ->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $formEvent) {
+            $data = $formEvent->getData();
+            $data[self::SLUG] = Str::softSlug($data[self::SLUG]);
+
+            $formEvent->setData($data);
+        });
         $builder->add('content', TextareaType::class, [
             'label' => 'trans.Content',
         ]);

bf63044660f82ca106ac32afb0f77665e8a4b0f0
LPodolski <m@lpodolski.com>
2019-05-21 05:57:34 +0200

    cron semaphore

--- symfony/src/Service/Cron/CronService.php
+++ symfony/src/Service/Cron/CronService.php
@@ -6,7 +6,9 @@ namespace App\Service\Cron;
 
 use App\Entity\SystemLog;
 use App\Service\System\SystemLog\SystemLogService;
+use App\System\Lock\AppLockInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Lock\Factory;
 
 class CronService
 {
@@ -20,18 +22,34 @@ class CronService
      */
     private $systemLogService;
 
-    public function __construct(EntityManagerInterface $em, SystemLogService $systemLogService)
+    /**
+     * @var Factory
+     */
+    private $lockFactory;
+
+    public function __construct(EntityManagerInterface $em, SystemLogService $systemLogService, Factory $lockFactory)
     {
         $this->em = $em;
         $this->systemLogService = $systemLogService;
+        $this->lockFactory = $lockFactory;
     }
 
     public function run(): void
     {
-        $this->updateFeatured();
-        $this->setMainImage();
+        $lock = $this->lockFactory->createLock(AppLockInterface::CRON, 3600);
 
-        $this->systemLogService->addSystemLog(SystemLog::CRON_RUN_TYPE, "cron executed");
+        if (!$lock->acquire()) {
+            return;
+        }
+
+        try {
+            $this->updateFeatured();
+            $this->setMainImage();
+
+            $this->systemLogService->addSystemLog(SystemLog::CRON_RUN_TYPE, "cron executed");
+        } finally {
+            $lock->release();
+        }
     }
 
     private function updateFeatured(): void

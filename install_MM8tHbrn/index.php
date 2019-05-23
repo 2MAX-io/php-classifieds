7e0548fa5dcadbf75e58f37dc87538441df4570b
LPodolski <m@lpodolski.com>
2019-05-23 19:21:16 +0200

    check file writable before install

--- install_MM8tHbrn/index.php
+++ install_MM8tHbrn/index.php
@@ -2,6 +2,25 @@
 
 declare(strict_types=1);
 
+use App\System\Filesystem\FilesystemChecker;
+
+require dirname(__DIR__) . '/symfony/vendor/autoload.php';
+
+if (count(FilesystemChecker::notWritableFileList())) {
+    echo 'Some files are not writable';
+    exit;
+}
+
+if (count(FilesystemChecker::creatingDirFailedList())) {
+    echo 'some dirs can not be created';
+    exit;
+}
+
+if (count(FilesystemChecker::writingFileFailedList())) {
+    echo 'Writing test file to some dirs failed';
+    exit;
+}
+
 $pdo = new \PDO('mysql:host=mysql;dbname=dev_test', 'root', '', [
     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
     \PDO::ATTR_EMULATE_PREPARES => true,
---
+++ symfony/src/System/Filesystem/FilesystemChecker.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\System\Filesystem;
+
+use App\Helper\FilePath;
+use Symfony\Component\Finder\Finder;
+
+class FilesystemChecker
+{
+    public static function notWritableFileList(): array
+    {
+        $return = [];
+
+        $finder = new Finder();
+        $finder->in(FilePath::getProjectDir());
+        $finder->exclude([
+            'static',
+            'symfony/docker/mysql/data/',
+        ]);
+
+        $files = $finder->files()->getIterator();
+        $i = 0;
+        foreach ($files as $file) {
+            ++$i;
+            if ($i > 1000) {
+                break;
+            }
+
+            if (!is_writable($file->getPath())) {
+                $return[] = $file->getPath();
+            }
+        }
+
+        return $return;
+    }
+
+    public static function creatingDirFailedList(): array
+    {
+        $return = [];
+        $patchList = [
+            FilePath::getProjectDir() . '/symfony/var/cache/prod_test',
+            FilePath::getProjectDir() . '/symfony/var/cache/prod/test',
+            FilePath::getProjectDir() . '/symfony/var/cache/log/test',
+            FilePath::getProjectDir() . '/static/cache/test',
+            FilePath::getProjectDir() . '/static/cache/listing/test',
+            FilePath::getProjectDir() . '/static/listing/test',
+            FilePath::getProjectDir() . '/static/resized/test',
+        ];
+
+        foreach ($patchList as $patch) {
+            if (!FilesystemChecker::canCreateDir($patch)) {
+                $return[] = $patch;
+            }
+        }
+
+        return $return;
+    }
+
+    public static function writingFileFailedList(): array
+    {
+        $return = [];
+        $patchList = [
+            FilePath::getProjectDir() . '/static/cache/',
+            FilePath::getProjectDir() . '/static/category/',
+            FilePath::getProjectDir() . '/static/listing/',
+            FilePath::getProjectDir() . '/static/logo/',
+            FilePath::getProjectDir() . '/static/resized/',
+            FilePath::getProjectDir() . '/symfony/var/cache',
+            FilePath::getProjectDir() . '/symfony/var/cache/upgrade',
+            FilePath::getProjectDir() . '/symfony/var/cache/prod',
+            FilePath::getProjectDir() . '/symfony/var/log',
+        ];
+
+        foreach ($patchList as $patch) {
+            if (!FilesystemChecker::canWriteFile($patch)) {
+                $return[] = $patch;
+            }
+        }
+
+        return $return;
+    }
+
+    public static function canWriteFile(string $path): bool
+    {
+        try {
+            $testFilePath = $path . '/' . 'test_safe_to_delete.txt';
+            $result = \file_put_contents($testFilePath, 'test, can safely delete');
+
+            $deleteResult = \unlink($testFilePath);
+
+            if (false === $result || $deleteResult === false) {
+                return false;
+            }
+        } catch (\Throwable $e) {
+            return false;
+        } finally {
+            \unlink($testFilePath);
+        }
+
+        return true;
+    }
+
+    public static function canCreateDir(string $path): bool
+    {
+        try {
+            $result = \mkdir($path);
+            $deleteResult = \rmdir($path);
+
+            if (false === $result || $deleteResult === false) {
+                return false;
+            }
+        } catch (\Throwable $e) {
+            return false;
+        } finally {
+            \rmdir($path);
+        }
+
+        return true;
+    }
+}

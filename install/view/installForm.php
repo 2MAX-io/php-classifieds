14bd92c414f49922fd7e1e16d06e071cda41d765
LPodolski <m@lpodolski.com>
2019-05-25 07:57:40 +0200

    install - allow rerun installation

--- install/view/installForm.php
+++ install/view/installForm.php
@@ -4,35 +4,41 @@
 
 <div class="container">
 
-    <form action="install.php" method="post">
+    <form action="" method="post">
         <h1>Installation</h1>
+
+        <?php foreach ($errors as $error): ?>
+            <div class="alert alert-danger">
+                <?php echo escape($error) ?>
+            </div>
+        <?php endforeach; ?>
 
         <div class="mb-4">
             <h3>Database:</h3>
 
             <div class="form-group">
                 <label>DB host</label>
-                <input name="db_host" value="localhost" type="text" placeholder="DB host" class="form-control" required>
+                <input name="db_host" value="<?php echo escape($_POST['db_host'] ?? 'localhost') ?>" type="text" placeholder="DB host" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>DB port</label>
-                <input name="db_port" value="3306" type="text" placeholder="DB port" class="form-control" required>
+                <input name="db_port" value="<?php echo escape($_POST['db_port'] ?? '3306') ?>" type="text" placeholder="DB port" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>DB database name</label>
-                <input name="db_name" type="text" placeholder="database_name" class="form-control" required>
+                <input name="db_name" value="<?php echo escape($_POST['db_name'] ?? '') ?>" type="text" placeholder="database_name" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>DB username</label>
-                <input name="db_user" type="text" placeholder="username" class="form-control" required>
+                <input name="db_user" value="<?php echo escape($_POST['db_user'] ?? '') ?>" type="text" placeholder="username" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>DB password</label>
-                <input name="db_pass" type="text" placeholder="password" class="form-control">
+                <input name="db_pass" value="<?php echo escape($_POST['db_pass'] ?? '') ?>" type="text" placeholder="password" class="form-control">
             </div>
         </div>
 
@@ -43,7 +49,7 @@
                 <label>Timezone</label>
                 <select name="app_timezone" type="text" class="form-control">
                     <?php foreach (\DateTimeZone::listIdentifiers() as $timezone): ?>
-                    <option <?php echo $timezone === 'UTC' ? 'selected' : '' ?> value="<?php echo $timezone ?>"><?php echo $timezone ?></option>
+                    <option <?php echo $timezone === ($_POST['app_timezone'] ?? 'UTC') ? 'selected' : '' ?> value="<?php echo $timezone ?>"><?php echo $timezone ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
@@ -54,22 +60,22 @@
 
             <div class="form-group">
                 <label>SMTP host</label>
-                <input name="smtp_host" type="text" placeholder="mail.host.com" class="form-control" required>
+                <input name="smtp_host" value="<?php echo escape($_POST['smtp_host'] ?? '') ?>" type="text" placeholder="mail.host.com" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>SMTP port</label>
-                <input name="smtp_port" value="465" type="text" placeholder="465" class="form-control" required>
+                <input name="smtp_port" value="<?php echo escape($_POST['smtp_port'] ?? '465') ?>" type="text" placeholder="465" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>SMTP username</label>
-                <input name="smtp_username" type="text" placeholder="user@mail.com" class="form-control" required>
+                <input name="smtp_username" value="<?php echo escape($_POST['smtp_username'] ?? '') ?>" type="text" placeholder="user@mail.com" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>SMTP password</label>
-                <input name="smtp_password" type="text" placeholder="password" class="form-control" required>
+                <input name="smtp_password" value="<?php echo escape($_POST['smtp_password'] ?? '') ?>" type="text" placeholder="password" class="form-control" required>
             </div>
         </div>
 
@@ -78,12 +84,12 @@
 
             <div class="form-group">
                 <label>Admin email</label>
-                <input name="admin_email" type="email" placeholder="admin@classified.com" class="form-control" required>
+                <input name="admin_email" value="<?php echo escape($_POST['admin_email'] ?? '') ?>" type="email" placeholder="admin@classified.com" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label>Admin password</label>
-                <input name="admin_password" type="text" placeholder="password" class="form-control" required>
+                <input name="admin_password" value="<?php echo escape($_POST['admin_password'] ?? '') ?>" type="text" placeholder="password" class="form-control" required>
             </div>
         </div>
 

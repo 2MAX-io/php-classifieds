60a49474fc316365a9d28e1478399a5716a70072
LPodolski <m@lpodolski.com>
2019-05-25 09:29:45 +0200

    install - increase time and memory limit

--- install/index.php
+++ install/index.php
@@ -12,6 +12,8 @@ use Webmozart\PathUtil\Path;
 
 ini_set('display_errors', '1');
 error_reporting(-1);
+set_time_limit(600);
+ini_set('memory_limit', '-1');
 
 if (version_compare(PHP_VERSION, '7.3', '<')) {
     echo 'This app requires PHP 7.3';

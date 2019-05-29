fffe2fda8a071b70c21f267618dd56ea67078da9
LPodolski <m@lpodolski.com>
2019-05-29 17:08:47 +0200

    install link to documentation about requirements

--- install/view/php_requirements.php
+++ install/view/php_requirements.php
@@ -6,6 +6,8 @@
 
     <h1>Installation - Server and PHP requirements</h1>
 
+    <div class="alert alert-info">You can find more information about requirements in <a href="https://documentation.2max.io/display/CLAS/Requirements">documentation</a></div>
+
     <?php foreach ($errors as $error): ?>
         <div class="alert alert-danger">
             <?php echo escape($error) ?>

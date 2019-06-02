6d93f0f4bd8e2b90fbd926e833d968e9b93f0977
LPodolski <m@lpodolski.com>
2019-06-02 18:31:19 +0200

    code review - form types

--- zz_engine/src/Form/Type/AdminCategoryType.php
+++ zz_engine/src/Form/Type/AdminCategoryType.php
@@ -6,12 +6,23 @@ namespace App\Form\Type;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdminCategoryType extends AbstractType
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
@@ -34,8 +45,8 @@ class AdminCategoryType extends AbstractType
 
                 return join(' ⇾ ', $path);
             },
-            'query_builder' => function (CategoryRepository $categoryRepository) {
-                $qb = $categoryRepository->createQueryBuilder('category');
+            'query_builder' => function () {
+                $qb = $this->em->getRepository(Category::class)->createQueryBuilder('category');
                 $qb->addOrderBy('category.sort', 'ASC');
 
                 return $qb;
--- zz_engine/src/Form/Type/CategoryType.php
+++ zz_engine/src/Form/Type/CategoryType.php
@@ -6,12 +6,23 @@ namespace App\Form\Type;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
@@ -37,8 +48,8 @@ class CategoryType extends AbstractType
 
                 return join(' ⇾ ', $path);
             },
-            'query_builder' => function (CategoryRepository $categoryRepository) {
-                $qb = $categoryRepository->createQueryBuilder('category');
+            'query_builder' => function () {
+                $qb = $this->em->getRepository(Category::class)->createQueryBuilder('category');
 
                 $qb->andWhere('category.lvl > 1');
                 $qb->addOrderBy('category.sort', 'ASC');

8f99e8df7b8b3a71a3b3cf817cbdeb9dfea013bd
LPodolski <m@lpodolski.com>
2019-06-08 10:36:07 +0200

    file upload better security

--- zz_engine/src/Controller/Pub/Secondary/FileUploadController.php
+++ zz_engine/src/Controller/Pub/Secondary/FileUploadController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Pub\Secondary;
 
+use App\Helper\FileHelper;
 use App\Helper\FilePath;
 use App\Service\System\FileUpload\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,16 +19,19 @@ class FileUploadController extends AbstractController
      */
     public function uploadFile(Filesystem $filesystem): Response
     {
+        $fileKey = 'files';
+        foreach ($_FILES[$fileKey]['name'] as $fileName) {
+            FileHelper::throwExceptionIfUnsafeFilename($fileName);
+        }
+
         $tmpUploadDir = FilePath::getTempFileUpload() . '/' . date('Y') . '/' . date('m') . '/' . date('d') . '/';
         $filesystem->mkdir($tmpUploadDir, 0770);
-        $fileUploader = new FileUploader(
-            'files', [
-                'uploadDir' => $tmpUploadDir,
-                'extensions' => ['jpg', 'jpeg', 'png'],
-                'fileMaxSize' => 2,
-                'title' => ['zz_file_upload_' . date('Ymd_His') . '_{random}.{extension}', 32]
-            ]
-        );
+        $fileUploader = new FileUploader($fileKey, [
+            'uploadDir' => $tmpUploadDir,
+            'extensions' => ['jpg', 'jpeg', 'png'],
+            'fileMaxSize' => 2,
+            'title' => ['zz_file_upload_' . date('Ymd_His') . '_{random}.{extension}', 32]
+        ]);
         $response = $fileUploader->upload();
 
         return $this->json($response);

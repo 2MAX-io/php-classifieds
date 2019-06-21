ddf736b8378b3608e98967cbf9e9b74fccf63309
LPodolski <m@lpodolski.com>
2019-06-21 12:53:25 +0200

    install listing demo - fix missing main image

--- install/data/example/listing.large.sql
+++ install/data/example/listing.large.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e7895f6c4ad522ce5acec073994d94944867df94552517a4f245cbb9edcc549
-size 3415059
+oid sha256:02bbad2f3ebf6057058aa70d91bcc5f0742ffed91dfe4a1e25972c6e5826447b
+size 3486520

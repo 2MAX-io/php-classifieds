14772c2fe86b4a5e7dc677e5bebcb16c79889ed7
LPodolski <m@lpodolski.com>
2020-12-08 09:32:35 +0100

    files / image move to new location

---
+++ zz_engine/src/Helper/ListingFileHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+use App\Entity\Listing;
+
+class ListingFileHelper
+{
+    public static function getDestinationDirectory(Listing $listing): string
+    {
+        $userDivider = \floor($listing->getUserNotNull()->getId() / 10000) + 1;
+
+        return FilePath::getListingFilePath()
+            . '/'
+            . $userDivider
+            . '/'
+            . 'user_' . $listing->getUserNotNull()->getId()
+            . '/'
+            . 'listing_' . $listing->getId();
+    }
+}

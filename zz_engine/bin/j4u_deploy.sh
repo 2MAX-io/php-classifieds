a008cb22769cc41539e322b536b1ee81b55fbccc
LPodolski <m@lpodolski.com>
2021-02-04 05:11:56 +0100

    j4u better deploy

--- zz_engine/bin/j4u_deploy.sh
+++ zz_engine/bin/j4u_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 set -o xtrace
+set -e
 
 cd /home/j4uoglosze/domains/ogloszenia.jaslo4u.pl/public_html
 git pull
@@ -10,3 +13,5 @@ wget -O /dev/null https://www.ogloszenia.jaslo4u.pl/
 /usr/local/php73/bin/php /home/j4uoglosze/domains/ogloszenia.jaslo4u.pl/public_html/zz_engine/composer.phar install -d zz_engine --classmap-authoritative
 
 wget -O /dev/null https://www.ogloszenia.jaslo4u.pl/
+
+printf "${GREEN}CONSIDER IT DONE${NC}\n"

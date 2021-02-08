6161b3d85de7837c29f7875de0a6a7bfe21cac16
LPodolski <m@lpodolski.com>
2021-02-08 06:28:14 +0100

    j4u update deploy to exec migrations

--- zz_engine/bin/j4u_deploy.sh
+++ zz_engine/bin/j4u_deploy.sh
@@ -11,6 +11,7 @@ rm -r /home/j4uoglosze/domains/ogloszenia.jaslo4u.pl/public_html/zz_engine/var/c
 wget -O /dev/null https://www.ogloszenia.jaslo4u.pl/
 
 /usr/local/php73/bin/php /home/j4uoglosze/domains/ogloszenia.jaslo4u.pl/public_html/zz_engine/composer.phar install -d zz_engine --classmap-authoritative
+/usr/local/php73/bin/php zz_engine/bin/console doctrine:migrations:migrate --all-or-nothing --no-interaction
 
 wget -O /dev/null https://www.ogloszenia.jaslo4u.pl/
 

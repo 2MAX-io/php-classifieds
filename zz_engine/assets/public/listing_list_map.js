7cd99299489564ce3a31d88d9daef8992eefde17
LPodolski <m@lpodolski.com>
2021-03-06 09:43:55 +0100

    listing list map

--- zz_engine/assets/public/listing_list_map.js
+++ zz_engine/assets/public/listing_list_map.js
@@ -14,59 +14,61 @@ import "prunecluster/dist/LeafletStyleSheet.css";
 import markerIcon from "~/module/leaflet/leaflet_marker_icon";
 
 let map = new Map();
-let leafletMap = map.createLeafletMap(document.querySelector(".js__mapWithListings"));
+let mapElement = document.querySelector(".js__mapWithListings");
+if (mapElement) {
+    let leafletMap = map.createLeafletMap(mapElement);
+    leafletMap.on("moveend", function () {
+        let url = new URL(window.location);
+        let lat = leafletMap.getCenter().lat;
+        let lng = leafletMap.getCenter().lng;
+        let zoom = leafletMap.getZoom();
 
-leafletMap.on("moveend", function () {
-    let url = new URL(window.location);
-    let lat = leafletMap.getCenter().lat;
-    let lng = leafletMap.getCenter().lng;
-    let zoom = leafletMap.getZoom();
+        url.searchParams.set(ParamEnum.LATITUDE, lat);
+        url.searchParams.set(ParamEnum.LONGITUDE, lng);
+        url.searchParams.set(ParamEnum.ZOOM, zoom);
+        window.history.pushState(null, null, url.toString());
 
-    url.searchParams.set(ParamEnum.LATITUDE, lat);
-    url.searchParams.set(ParamEnum.LONGITUDE, lng);
-    url.searchParams.set(ParamEnum.ZOOM, zoom);
-    window.history.pushState(null, null, url.toString());
+        document.querySelectorAll(".js__linkToMap").forEach((linkElement) => {
+            let linkUrl = new URL(linkElement.getAttribute("href"), window.location);
+            linkUrl.searchParams.set(ParamEnum.LATITUDE, lat);
+            linkUrl.searchParams.set(ParamEnum.LONGITUDE, lng);
+            linkUrl.searchParams.set(ParamEnum.ZOOM, zoom);
+            linkElement.setAttribute("href", linkUrl.toString());
+        });
 
-    document.querySelectorAll(".js__linkToMap").forEach((linkElement) => {
-        let linkUrl = new URL(linkElement.getAttribute("href"), window.location);
-        linkUrl.searchParams.set(ParamEnum.LATITUDE, lat);
-        linkUrl.searchParams.set(ParamEnum.LONGITUDE, lng);
-        linkUrl.searchParams.set(ParamEnum.ZOOM, zoom);
-        linkElement.setAttribute("href", linkUrl.toString());
+        document.querySelector(".js__filterLatitude").value = lat;
+        document.querySelector(".js__filterLongitude").value = lng;
+        document.querySelector(".js__filterZoom").value = zoom;
     });
 
-    document.querySelector(".js__filterLatitude").value = lat;
-    document.querySelector(".js__filterLongitude").value = lng;
-    document.querySelector(".js__filterZoom").value = zoom;
-});
-
-var pruneCluster = new PruneClusterForLeaflet();
-pruneCluster.PrepareLeafletMarker = function (leafletMarker, data) {
-    leafletMarker.setIcon(markerIcon);
-    let listingOnMap = data.listingOnMap;
-    let url = Routing.generate("app_listing_show", {
-        id: listingOnMap.listingId,
-        slug: listingOnMap.listingSlug,
-    });
-    let popupContent = `
+    var pruneCluster = new PruneClusterForLeaflet();
+    pruneCluster.PrepareLeafletMarker = function (leafletMarker, data) {
+        leafletMarker.setIcon(markerIcon);
+        let listingOnMap = data.listingOnMap;
+        let url = Routing.generate("app_listing_show", {
+            id: listingOnMap.listingId,
+            slug: listingOnMap.listingSlug,
+        });
+        let popupContent = `
         <a href='${url}'>${listingOnMap.listingTitle}</a>
     `;
-    if (leafletMarker.getPopup()) {
-        leafletMarker.setPopupContent(popupContent);
-    } else {
-        leafletMarker.bindPopup(popupContent);
-    }
-};
+        if (leafletMarker.getPopup()) {
+            leafletMarker.setPopupContent(popupContent);
+        } else {
+            leafletMarker.bindPopup(popupContent);
+        }
+    };
 
-dataForJs[ParamEnum.LISTING_LIST].forEach((listingOnMap) => {
-    var marker = new PruneCluster.Marker(listingOnMap.latitude, listingOnMap.longitude);
-    /**
-     * @typedef {Object} listingOnMap
-     * @property {string} listingSlug
-     * @property {string} listingTitle
-     */
-    marker.data.listingOnMap = listingOnMap;
-    pruneCluster.RegisterMarker(marker);
-});
+    dataForJs[ParamEnum.LISTING_LIST].forEach((listingOnMap) => {
+        var marker = new PruneCluster.Marker(listingOnMap.latitude, listingOnMap.longitude);
+        /**
+         * @typedef {Object} listingOnMap
+         * @property {string} listingSlug
+         * @property {string} listingTitle
+         */
+        marker.data.listingOnMap = listingOnMap;
+        pruneCluster.RegisterMarker(marker);
+    });
 
-leafletMap.addLayer(pruneCluster);
+    leafletMap.addLayer(pruneCluster);
+}

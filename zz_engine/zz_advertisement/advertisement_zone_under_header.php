f83efe0b199de4d4ad99501e1fa23dd2cae6c027
LPodolski <m@lpodolski.com>
2021-03-19 07:40:00 +0100

    info about license

--- zz_engine/zz_advertisement/advertisement_zone_under_header.php
+++ zz_engine/zz_advertisement/advertisement_zone_under_header.php
@@ -11,10 +11,20 @@ if (null !== $advertisementDto->category) {
     $zoneId = $advertisementDto->category->getAdvertisementZoneId();
 }
 
-if (empty($zoneId)) {
-    return; // skip displaying ad
-}
+//if (empty($zoneId)) {
+//    return; // skip displaying ad
+//}
 
 ?>
 
 <!-- ad zone id: <?php echo StringHelper::escape((string) $zoneId); ?> -->
+
+<!-- DO NOT REMOVE INFORMATION BELLOW WITHOUT HAVING VALID LICENSE -->
+<!-- information bellow can only be removed when having valid license for 2MAX.io PHP Classified Ads -->
+<div class="container-fluid container-lg">
+    <div class="alert alert-dark my-4">
+        <h1>Preview version of 2MAX.io PHP Classified Ads</h1>
+        <div class="mb-2">for commercial use you need to buy license</div>
+        <a href="https://php-classified-ads.2max.io/">Buy license for 2MAX.io PHP Classified Ads here</a>
+    </div>
+</div>
